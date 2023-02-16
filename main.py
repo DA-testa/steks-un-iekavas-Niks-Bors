7794b508ea815ba6d4a1f0d1793e96d1b375b6c8
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-02-16 21:59:38 +0000

    -

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # python3
-# 
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
-
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
@@ -13,17 +11,15 @@ def find_mismatch1(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
-            
-           
+            opening_brackets_stack.append(Bracket(next, i+1))
             # Process opening bracket, write your code here
-            
-          
-        if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-             return i+1
-            opening_brackets_stack.pop()
 
+        if next in ")]}":
+            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
+        
         if i == len(text) - 1 and len(opening_brackets_stack) == 0:
             return "Success"
     return 1
@@ -32,39 +28,29 @@ def find_mismatch2(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
-            
-           
-            
-            
-          
+            opening_brackets_stack.append(Bracket(next, i+1))
+            # Process opening bracket, write your code here
+
         if next in ")]}":
+            # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-             return i+1
+                return i+1
             opening_brackets_stack.pop()
             if opening_brackets_stack:
                 return opening_brackets_stack[0].position
-
-
             return "Success"
-    
-            
-    
-            
 
 
 def main():
     type = input()
-    if type =='F':
+    if type == 'F':
         fileName = input()
         file = open(fileName, "r")
         print(find_mismatch2(file))
-    elif type =='I':
+    elif type == 'I':
         text = input()
         print(find_mismatch2(text))
-        
-    # Printing answer, write your code here
 
 
 if __name__ == "__main__":
-     main()
+    main()
