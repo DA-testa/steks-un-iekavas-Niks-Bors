e33230dd6e901ceac82d9e33e808cb1c6cda931f
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-02-16 21:24:36 +0000

    -

--- main.py
+++ main.py
@@ -23,20 +23,44 @@ def find_mismatch(text):
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
              return i+1
             opening_brackets_stack.pop()
-            if not opening_brackets_stack:
+            if i==len(text)-1 and len(opening_brackets_stack)==0:
+
                 return "Success"
     return 1
+    def find_mismatch2(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next,i+1))
+            
+           
+            # Process opening bracket, write your code here
+            
+          
+        if next in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+             return i+1
+            opening_brackets_stack.pop()
+            if opening_brackets_stack:
+                return opening_brackets_stack[0].position
+
+
+            return "Success"
+    
             
     
             
 
 
 def main():
-    #text = inputs()
-    #if "I" in text:
-    if type =="I"
+    text = input()
+    if type =="F"
+    fileName = input()
+    file = open(fileName,"r")
+    print(find_mismatch2(file))
+    elif type =="I":
         text = input()
-    print(find_mismatch(text))
+        print(find_mismatch2(text))
         
     # Printing answer, write your code here
 
