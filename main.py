eff6a116a3ffe48f5746637131019de403750401
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-02-16 22:10:50 +0000

    -

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
-def find_mismatch1(text):
+def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
@@ -28,25 +28,7 @@ def find_mismatch1(text):
             return "Success"
     return 1
 
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
-            
-           
-            
-            
-          
-        if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-             return i+1
-            opening_brackets_stack.pop()
-            if opening_brackets_stack:
-                return opening_brackets_stack[0].position
 
-
-            return "Success"
     
             
     
