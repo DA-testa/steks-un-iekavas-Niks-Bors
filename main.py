53136bd2f90fe29b7fb2fb1b8f8c74265d05acd5
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-02-19 11:22:33 +0000

    -

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def find_mismatch(text):
             # Process opening bracket, write your code here
             
           
-        if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-             return i+1
+            if next in ")]}":
+                if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                    return i+1
 
-            opening_brackets_stack.pop()
+                opening_brackets_stack.pop()
         if opening_brackets_stack:
-            return opening_brackets_stack[0].position
+             return opening_brackets_stack[0].position
         return "Success"
         # if i == len(text)-1 and len(opening_brackets_stack)==0:
         
@@ -37,7 +37,7 @@ def main():
     if "I" in text:
         text = input()
         mismatch = find_mismatch(text)
-        return(mismatch)
+        print(mismatch)
         
     # Printing answer, write your code here
 
