4a82eda89ef4ca92e028f0b8b75a50a54be856c6
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-02-19 11:41:31 +0000

    -

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
+            opening_brackets_stack.append(Bracket(next,i+1)) 
+            # pievieno stekam atverošo iekavu, un pozīciju
             
            
             # Process opening bracket, write your code here
@@ -22,22 +23,24 @@ def find_mismatch(text):
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
+                # pārbauda vai stekā ir atbilstoša aizverošā iekava atverošai iekavai, un vai tas ir tukšs
 
-            opening_brackets_stack.pop()
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position
-    return "Success"
-        # if i == len(text)-1 and len(opening_brackets_stack)==0:
+            opening_brackets_stack.pop() # ja sakrīt iekavas, tad tiek izņemts no steka
+        if i == len(text)-1 and len(opening_brackets_stack)==0:
+            # ja teksta rindā visas iekavas sakrīt, tad izprintē success
+
+            return "Success"
+     
         
-    # return 1
+    
 
 def main():
 
     text = input()
     if "I" in text:
         text = input()
-        mismatch = find_mismatch(text)
-        print(mismatch)
+        ms = find_mismatch(text)
+        print(ms)
         
     # Printing answer, write your code here
 
